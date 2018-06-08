3559cd8ae71eea45bf868f6c46a7e97e9fb28491
Haider Malik <haidermalik504@gmail.com>
2018-06-08 21:48:37 +0500

    adding bootstrap

--- src/app/hero/hero-list/hero-list.component.ts
+++ src/app/hero/hero-list/hero-list.component.ts
@@ -4,16 +4,13 @@ import { Hero } from '../hero';
 @Component({
   selector: 'app-hero-list',
   template: `
-    <tr>
-    <td [attr.colspan] ="colSpan"> Hello </td>
-    </tr>
+  <p>{{title}} </p>
+  <button class="btn btn-primary"> Submit </button>
   `,
   styleUrls: ['./hero-list.component.css']
 })
 export class HeroListComponent implements OnInit {
-
   title: string;
-  colSpan = 2;
   constructor() {
     this.title = 'Hero List'
    }
