5cbb5aa4f11acaadb6e181689c7b12aaf06ff566
Haider Malik <haidermalik504@gmail.com>
2018-06-09 00:44:29 +0500

    pipes

--- src/app/hero/hero-list/hero-list.component.ts
+++ src/app/hero/hero-list/hero-list.component.ts
@@ -4,20 +4,24 @@ import { Hero } from '../hero';
 @Component({
   selector: 'app-hero-list',
   template: `
-  <p>{{title}} </p>
-  <p> <input [(ngModel)] ="name"/> </p>
-  <button class="btn btn-primary" (click) ="onSave()">Save </button>
+  <p> {{hero.name | uppercase}} </p>
+  <p> {{hero.salary | currency}} </p>
+  <p> {{hero.rating | number}} </p>
+  <p> {{hero.joinDate | date:'shortDate'}} </p>
+
   `,
   styleUrls: ['./hero-list.component.css']
 })
 export class HeroListComponent implements OnInit {
   title: string;
-  name = 'Kevin'
+  hero = {
+    rating: 3.45230,
+    name : 'Jane',
+    salary: 23234.9,
+    joinDate: new Date()
+  }
   constructor() {
     this.title = 'Hero List'
-   }
-   onSave(name){
-     console.log(this.name)
    }
   ngOnInit() {
   }
