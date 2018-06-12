80452205b8ab7ffeb7892347c5212aaad1e105aa
Haider Malik <haidermalik504@gmail.com>
2018-06-13 00:58:01 +0500

    ngSwitch starter

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,6 +8,34 @@ import { HeroService } from './hero/hero.service';
 })
 export class AppComponent {
   title = 'Angular Fundamentals';
+  people: any[] = [
+    {
+      "name": "Douglas  Pace",
+      "age": 35,
+      "country": 'MARS'
+    },
+    {
+      "name": "Mcleod  Mueller",
+      "age": 32,
+      "country": 'USA'
+    },
+    {
+      "name": "Day  Meyers",
+      "age": 21,
+      "country": 'HK'
+    },
+    {
+      "name": "Aguirre  Ellis",
+      "age": 34,
+      "country": 'UK'
+    },
+    {
+      "name": "Cook  Tyson",
+      "age": 32,
+      "country": 'USA'
+    }
+  ];
   constructor(private heroService: HeroService){
   }
+
 }
